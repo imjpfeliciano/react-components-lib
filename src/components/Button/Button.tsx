f71e4ffbc11912bc244bc797117f21117c97607b
Jonathan Peraza <jonathan_peraza@humanapi.co>
2022-10-06 19:57:57 -0500

    feat: library release

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,12 +1,13 @@
 import { StyledButton } from "./Button.styled";
+import React from "react";
 import { ButtonProps } from "./Button.types";
 
 const Button: React.FC<ButtonProps> = ({ text, onClick }) => {
   return <StyledButton onClick={onClick}>{text}</StyledButton>;
 };
 
-// const Button = ({ text, onButtonClick }: any) => {
-//   return <button onClick={onButtonClick}>{text}</button>;
+// const Button: React.FC<ButtonProps> = ({ text, onClick }) => {
+//   return <button onClick={onClick}>{text}</button>;
 // };
 
 export default Button;
